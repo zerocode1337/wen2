6e4ea87b1d8fb0bbda90ad3a675e0d2dcc570817
admin@yunshipei.com <adminyunshipeicom@amserver-01.local>
2016-04-11 18:29:27 +0800

    updata @2016.4.11

--- electron.js
+++ electron.js
@@ -3,12 +3,12 @@
 const electron = require('electron');
 const app = electron.app; //Module to control application life.
 
-//Module to store and restore window size and positions.
+// Module to store and restore window size and positions.
 const windowStateKeeper = require('electron-window-state');
 const BrowserWindow = electron.BrowserWindow; //Module to create native browser window.
 
-//Module to create native menus that can be used as application menus and context menus.
-const Menu = require('menu');
+// Module to create native menus that can be used as application menus and context menus.
+const menu = require('menu');
 
 const path = require('path');
 //if(process.env.NODE_ENV === 'dev'){
@@ -17,34 +17,76 @@ const path = require('path');
 //  });
 //}
 
-//Start Servers
+var menuTemplate = [{
+    label: '应用',
+    submenu: [{
+        label: '退出',
+        accelerator: 'CmdOrCtrl+Q',
+        click: function(){
+             app.quit();
+        }
+    }, {
+        label: '最小化',
+        accelerator: 'CmdOrCtrl+H',
+        click: function(){
+             app.hide();
+        }
+    }, {
+        label: '最大化',
+        accelerator: 'CmdOrCtrl+S',
+        click: function(){
+             app.show();
+        }
+    }]
+}, {
+    label: '编辑',
+    submenu: [{
+        label: '剪切',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut:'
+    }, {
+        label: '拷贝',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy:'
+    }, {
+        label: '粘贴',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste:'
+    }]
+}];
+
+// Start Servers
 require('./server');
 
 
-//Keep a global reference of the window object, if you don't, the window will
-//be closed automatically when the JavaScript object is garbage collected.
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
 
-//Quit when all windows are closed.
+// Quit when all windows are closed.
 app.on('window-all-closed', function(){
-    //On OS x it is common for applications and their menu bar
-    //to stay active until the user quits explicitly with cmd+Q
+    // On OS x it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with cmd+Q
     if (process.platform != 'darwin') {
         app.quit();
     }
 });
 
 /*
- *This method will be called when Electron has finished
- *initialization and is ready to create browser windows.
+ * This method will be called when Electron has finished
+ * initialization and is ready to create browser windows.
  */
 app.on('ready', function() {
-    //Load the previous state with fallback to defaluts
+
+    // Load the menu
+    menu.setApplicationMenu(menu.buildFromTemplate(menuTemplate));
+
+    // Load the previous state with fallback to defaluts
     let windowState = windowStateKeeper('main', {
         width : 1800,
         height: 600
     });
-    //Create the browser window.
+    // Create the browser window.
     mainWindow = new BrowserWindow({
         x        : windowState.x,
         y        : windowState.y,
