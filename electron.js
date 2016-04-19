da5bdd481b4529a8aef9acecac7b31a4516e48fe
kevin <zerocode1337>
2016-04-19 18:38:02 +0800

    change const to var at electron.js

--- electron.js
+++ electron.js
@@ -1,11 +1,10 @@
 'use strict';
 
-const electron = require('electron');
-const app = electron.app; //Module to control application life.
+var app = require('app'); //Module to control application life.
 
 // Module to store and restore window size and positions.
 const windowStateKeeper = require('electron-window-state');
-const BrowserWindow = electron.BrowserWindow; //Module to create native browser window.
+var BrowserWindow = require('browser-window'); //Module to create native browser window.
 
 // Module to create native menus that can be used as application menus and context menus.
 const menu = require('menu');
